f93d185b0e3b99d7bdbc132331aae82bbd49d72b
yohann <yohann@thinkr.fr>
2024-11-27 10:44:23 +0100

    ajout selectbox

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ import duckdb
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 
-st.write("Hello, world!")
-st.header("This is a header")
+st.write("App pour faire du SQL")
+
+option = st.selectbox(
+    "What would you like to review?",
+    ("Joins", "Group by", "Windows functions"),
+    index=None,
+    placeholder="Select something...",
+)
+
+st.write("You selected:", option)
+
 input_text = st.text_area("Please enter your sql")
 st.dataframe(df)
 
